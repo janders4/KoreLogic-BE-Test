ba9da67adf97e78a8c5829b037e7a4a3f624b9c4
janders4 <johnanderson198689@gmail.com>
2019-08-13 15:52:56 +0200

    /resauraunts ability to accept multiple queries implemented

--- models/restaurantModels.js
+++ models/restaurantModels.js
@@ -1,13 +1,21 @@
 const restaurantData = require("../cafe.json");
 
-exports.fetchAllRestaurants = () => {
-  return restaurantData;
+exports.fetchAllRestaurants = params => {
+  if (!params) return restaurantData;
+  const restraunts = restaurantData.filter(item => {
+    for (let key in params) {
+      if (item[key] != JSON.parse(params[key])) return false;
+    }
+    return true;
+  });
+  if (restraunts.length === 0) throw new Error("No restaurants found");
+  return restraunts;
 };
 
 exports.fetchRestaurantById = id => {
   const restaurant = restaurantData.filter(restaurant => {
     return restaurant.name === id;
   });
-  if (restaurant.length === 0) throw new Error("Restaurant not found");
+  if (result.length === 0) throw new Error("Restaurant not found");
   return restaurant;
 };
