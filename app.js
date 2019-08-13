2b2feac6e9e245b7d8b34b22936b01cb55044e8b
janders4 <johnanderson198689@gmail.com>
2019-08-13 15:32:25 +0200

    error handling added to handle restaurant id returning nothing

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ const { apiRouter } = require("./routers/apiRouters");
 
 app.use(express.json());
 app.use("/api", apiRouter);
+app.use((err, req, res, next) => {
+  res.status(404).json({
+    status: 404,
+    error: err.message
+  });
+});
 module.exports = app;
--- models/restaurantModels.js
+++ models/restaurantModels.js
@@ -8,5 +8,6 @@ exports.fetchRestaurantById = id => {
   const restaurant = restaurantData.filter(restaurant => {
     return restaurant.name === id;
   });
+  if (restaurant.length === 0) throw new Error("Restaurant not found");
   return restaurant;
 };
