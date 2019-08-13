def818c4c28420f73ba36347f4dcd8a409494041
janders4 <johnanderson198689@gmail.com>
2019-08-13 15:27:07 +0200

    /restraunts/:id route, controller & model implemented

--- models/restaurantModels.js
+++ models/restaurantModels.js
@@ -3,3 +3,10 @@ const restaurantData = require("../cafe.json");
 exports.fetchAllRestaurants = () => {
   return restaurantData;
 };
+
+exports.fetchRestaurantById = id => {
+  const restaurant = restaurantData.filter(restaurant => {
+    return restaurant.name === id;
+  });
+  return restaurant;
+};
--- routers/restaurantRouters.js
+++ routers/restaurantRouters.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { getAllRestaurants } = require("../controllers/restaurantController");
+const {
+  getAllRestaurants,
+  getRestaurantById
+} = require("../controllers/restaurantController");
 const restaurantsRouter = express.Router();
 
 restaurantsRouter.get("/", getAllRestaurants);
+restaurantsRouter.get("/:id", getRestaurantById);
 
 module.exports = { restaurantsRouter };
